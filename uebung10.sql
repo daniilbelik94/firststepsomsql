546a253acc070eb7f1845e378db5461352861274
Daniil Belik <daniilbelik@icloud.com>
2024-08-16 08:53:15 +0200

    Update uebung10.sql
    done aufgaben 10

--- uebung10.sql
+++ uebung10.sql
@@ -82,14 +82,63 @@ ON
 
 
 
- 2. -- Wechseln Sie zur Datenbank Uebungen. 
+-- Wechseln Sie zur Datenbank Uebungen. 
 -- Erstellen Sie eine Abfrage über die Tabellen t_lager und t_artikel, um alle am Lager vor-
 -- rätigen Artikel aufzulisten. Verwenden Sie für die Tabellen passende Ersatznamen und lassen 
 -- Sie sich die Artikelnummer, den Artikelnamen, den Lieferanten und die Stückzahl anzeigen. 
 
 
+source datenbank_uebungen.sql;
 
-use Uebungen;
+use uebungen;
 
-select t_lager as Nummer,
-t_artikel as Name,
+select t_lager.id as Artikelnummer,
+t_artikel.name as Artikelname,
+t_artikel.lieferant as Lieferant,
+t_lager.stueck as Stueckzahl
+from t_lager
+join t_artikel   
+ON t_lager.id = t_artikel.id;
+
+
+-- Wandeln Sie die Abfrage so um, dass Sie alle Artikel erhalten, egal ob Sie am Lager sind oder 
+-- nicht. Falls vorhanden, sollen neben der Artikelnummer, dem Artikelnamen und der Liefe-
+-- rantennummer auch die Stückzahl und der Preis angezeigt werden. 
+
+
+
+select t_artikel.id as Artikelnummer,
+t_artikel.name as Artikelname,
+t_artikel.lieferant as Lieferantennummer,
+t_lager.stueck as Stueckzahl,
+t_lager.preis as Preis
+from t_artikel
+left join t_lager
+on t_artikel.id = t_lager.id
+
+order by t_lager.stueck;
+
+
+--Erweitern Sie die Abfrage so, dass zusätzlich zu den bisherigen Feldern noch der Name der 
+-- Lieferanten zurückgeliefert wird. Dabei sollen nur die Artikel berücksichtigt werden, die am 
+-- Lager sind. Sortieren Sie die Ausgabe nach den Artikelnamen. 
+
+
+SELECT 
+    t_artikel.id AS Artikelnummer,
+    t_artikel.name AS Artikelname,
+    t_liefer.name AS Lieferantenname,
+    t_lager.stueck AS Stueckzahl,
+    t_lager.preis AS Preis
+FROM 
+    t_artikel
+JOIN 
+    t_lager ON t_artikel.id = t_lager.id
+JOIN 
+    t_liefer ON t_artikel.lieferant = t_liefer.id
+WHERE 
+    t_lager.stueck > 0
+ORDER BY 
+    t_artikel.name;
+
+
